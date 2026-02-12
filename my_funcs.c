815347360ebe2351c5b62f01bcc8a6f794143757
Mugi-wara-teriyala <kailesh.kj2507@gmail.com>
2026-02-13 00:06:31 +0530

    Merged remote and local repository

--- my_funcs.c
+++ my_funcs.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+        
 #include "my_funcs.h"
 #include <stdio.h>
 #include <string.h>
@@ -19,26 +19,4 @@ printf("The selected course outcome is:  CO%d\n", i+1);
                         }
                     printf("\n");
                 printf("+------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
-=======
-#include "my_funcs.h"
-#include <stdio.h>
-#include <string.h>
-void print_table(int i , po *PO)
-{
-printf("The selected course outcome is:  CO%d\n", i+1);
-                printf("+------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
-                printf("| CO   | PO1 | PO2 | PO3 | PO4 | PO5 | PO6 | PO7 | PO8 | PO9 | PO10| PO11|\n");
-                printf("+------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
-                    
-                printf("| CO%-2d |", i + 1);
-
-                    for (int j = 0; j < 11; j++) {
-                        if (PO[j].contribution[i] == 0)
-                            printf("  -  |");
-                        else
-                            printf("  %d  |", PO[j].contribution[i]);
-                        }
-                    printf("\n");
-                printf("+------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
->>>>>>> 557d44aed67b780c7dac8b70253a2eeaa4c6b3ac
 }
